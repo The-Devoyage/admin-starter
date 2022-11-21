19a28895ca2941aa0947760c76b6c4b2be5d9f51
ericmy123 <emyudin@gmail.com>
2022-11-21 13:16:12 -0500

    feature/Web Title with env var

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,45 @@
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin} from 'vite';
+//import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), 
+    createHtmlPlugin({
+      minify: true,
+      /**
+       * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
+       * @default src/main.ts
+       */
+      entry: 'src/main.tsx',
+      /**
+       * If you want to store `index.html` in the specified folder, you can modify it, otherwise no configuration is required
+       * @default index.html
+       */
+      template: 'index.html',
+
+      /**
+       * Data that needs to be injected into the index.html ejs template
+       */
+      inject: {
+        data: {
+          //title: import.meta.env.VITE_WEB_TITLE,
+          title: process.env.VITE_WEB_TITLE,
+        },
+        tags: [
+          {
+            injectTo: 'body-prepend',
+            tag: 'div',
+            attrs: {
+              id: 'tag',
+            },
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: [{ find: 'src', replacement: '/src' }],
   },
@@ -15,3 +51,5 @@ export default defineConfig({
     },
   },
 });
+
+
