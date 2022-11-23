f1bdaab6a939aa07b4a46ea50563883d2f3d2c0b
ericmy123 <emyudin@gmail.com>
2022-11-22 21:33:38 -0500

    feature/Web Title Env Var

--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,57 @@
-import { defineConfig, Plugin} from 'vite';
-//import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-  plugins: [react(), 
-    createHtmlPlugin({
-      minify: true,
-      /**
-       * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
-       * @default src/main.ts
-       */
-      entry: 'src/main.tsx',
-      /**
-       * If you want to store `index.html` in the specified folder, you can modify it, otherwise no configuration is required
-       * @default index.html
-       */
-      template: 'index.html',
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
-      /**
-       * Data that needs to be injected into the index.html ejs template
-       */
-      inject: {
-        data: {
-          //title: import.meta.env.VITE_WEB_TITLE,
-          title: process.env.VITE_WEB_TITLE,
-        },
-        tags: [
-          {
-            injectTo: 'body-prepend',
-            tag: 'div',
-            attrs: {
-              id: 'tag',
-            },
+
+  return defineConfig({
+    plugins: [react(), 
+      createHtmlPlugin({
+        minify: true,
+        /**
+         * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
+         * @default src/main.ts
+         */
+        entry: 'src/main.tsx',
+        /**
+         * If you want to store `index.html` in the specified folder, you can modify it, otherwise no configuration is required
+         * @default index.html
+         */
+        template: 'index.html',
+
+        /**
+         * Data that needs to be injected into the index.html ejs template
+         */
+        inject: {
+          data: {
+            title: process.env.VITE_WEB_TITLE,
           },
-        ],
-      },
-    }),
-  ],
-  resolve: {
-    alias: [{ find: 'src', replacement: '/src' }],
-  },
-  server: {
-    port: 3333,
-    proxy: {
-      '/graphql': 'http://localhost:5000',
-      '/media-server': 'http://localhost:5000',
+          tags: [
+            {
+              injectTo: 'body-prepend',
+              tag: 'div',
+              attrs: {
+                id: 'tag',
+              },
+            },
+          ],
+        },
+      }),
+    ],
+    resolve: {
+      alias: [{ find: 'src', replacement: '/src' }],
     },
-  },
-});
-
+    server: {
+      port: 3333,
+      proxy: {
+        '/graphql': 'http://localhost:5000',
+        '/media-server': 'http://localhost:5000',
+      },
+    },
+  });
+  }
 
