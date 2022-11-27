608173faa299ae96f0a69fb25444e1e93e27e1e6
ericmy123 <emyudin@gmail.com>
2022-11-27 13:38:18 -0500

    feature/Env-Var-Web-Title-updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,30 +2,14 @@ import { defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
-// https://vitejs.dev/config/
-
 export default ({ mode }) => {
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-
 
   return defineConfig({
     plugins: [react(), 
       createHtmlPlugin({
-        minify: true,
-        /**
-         * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
-         * @default src/main.ts
-         */
         entry: 'src/main.tsx',
-        /**
-         * If you want to store `index.html` in the specified folder, you can modify it, otherwise no configuration is required
-         * @default index.html
-         */
         template: 'index.html',
-
-        /**
-         * Data that needs to be injected into the index.html ejs template
-         */
         inject: {
           data: {
             title: process.env.VITE_WEB_TITLE,
